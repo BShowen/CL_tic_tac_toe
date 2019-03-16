39d81eac8bfc216ecf86cf8eedaa8a19606e7632
Bradley Showen <BShowen@me.com>
2019-03-16 07:40:29 -0400

    Added a turn method to player

--- player.rb
+++ player.rb
@@ -13,6 +13,17 @@ class Player
         store_position
     end
 
+    def turn 
+        # This is a loop because I am not done defining this method. This method (soon) will verify that the user has entered the correct input. If the user enters an undesired input then it will be ingored and this loop will start over, until they have entered the desired input. This will be implemented later on when im posotive that I will be keeping this method. For now this method works perfectly for my testing. 
+        @my_turn = true
+        while @my_turn 
+            puts "#{self.name}, choose a position on the board."
+            self.position = gets.chomp.to_i
+            puts `clear`
+            @my_turn = !@my_turn 
+        end
+    end
+
     private
     def store_position
         @all_positions << @position
