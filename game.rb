ece20b5680cce36e51c11b31b1c19e0d7904a3c7
Bradley Showen <BShowen@me.com>
2019-03-17 08:47:01 -0400

    add ability to replay a game
    I need to work on the "play_again" method. I threw it together really quickly. It works.

--- game.rb
+++ game.rb
@@ -1,24 +1,43 @@
 require "./player.rb"
 require "./game_board.rb"
 
-puts "Lets play.\nWho wnats to be X?"
-player_X = Player.new gets.chomp , 'X'
-puts "Who will be O?"
-player_O = Player.new gets.chomp , "O"
-puts `clear`
-board = GameBoard.new
-
-turn = Proc.new do | player |
-    player.turn board.positions_left
-    board.render_board player.character , player.position
-    return if board.winner_or_tie? player.all_positions , player.name
+def intro
+    puts "Lets play.\nWho wnats to be X?"
+    $player_X = Player.new gets.chomp , 'X'
+    puts "Who will be O?"
+    $player_O = Player.new gets.chomp , "O"
+    puts `clear`
+    $board = GameBoard.new
 end
 
-def next_turn player , &turn 
-    turn.call player
+$turn = Proc.new do | player |
+    player.turn $board.positions_left
+    $board.render_board player.character , player.position
+    play_again if $board.winner_or_tie? player.all_positions , player.name
 end
 
-loop do
-    next_turn player_X , &turn
-    next_turn player_O , &turn
+def my_turn player , &block
+    block.call player
 end
+
+def play_one_round
+    intro 
+    loop do
+        my_turn $player_X , &$turn
+        my_turn $player_O , &$turn
+    end
+end
+
+def play_again
+    sleep 1
+    puts "Want to play again? Y/N"
+    reply = gets.chomp.upcase
+    if reply == "Y"
+        play_one_round
+    else
+        puts "Thanks for playing!"
+        exit 
+    end
+end
+
+play_one_round
