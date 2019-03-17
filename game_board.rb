69139803e747df42752a058e41268259dce4cd36
Bradley Showen <BShowen@me.com>
2019-03-17 08:16:02 -0400

    refactor "winner_or_tie?" method
    The "winner_or_tie?" method is now responsible for checking for a winner and alerting who the winner is or if there is a tie.

--- game_board.rb
+++ game_board.rb
@@ -14,21 +14,28 @@ class GameBoard
         puts "\n #{@positions[0] } | #{@positions[1]} | #{@positions[2]} \n---|---|---\n #{@positions[3]} | #{@positions[4]} | #{@positions[5]} \n---|---|---\n #{@positions[6]} | #{@positions[7]} | #{@positions[8]}"
     end
 
-    def winner_or_tie? player_positions=nil , toggle
-        case toggle
-        when 'winner?'
-            solutions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-            solutions.any? do |sub_array|
-                sub_array.all? do |sub_array_item|
-                    player_positions.include? sub_array_item
-                end
+    def winner_or_tie? player_positions , name
+        solutions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        winner = solutions.any? do |sub_array|
+            sub_array.all? do |sub_array_item|
+                player_positions.include? sub_array_item
             end
-        when 'tie?'
-            positions.empty? 
+        end
+    
+        tie = positions_left.empty? 
+    
+        if winner
+            puts "#{name} won!"
+            true
+        elsif tie
+            puts "Its a tie!"
+            true
+        else
+            false
         end
     end
 
-    def positions 
+    def positions_left 
         @positions.select do |item|
             item.is_a? Integer
         end
