c6d3459855ba277fbc2dee665cc15fbd6158eb72
Bradley Showen <BShowen@me.com>
2019-03-17 06:55:33 -0400

    Add check for stalemate
    GameBoard now has the capability to check for a winner ot stalemate in the same method. I will refactor this to make it cleaner.

--- game_board.rb
+++ game_board.rb
@@ -14,12 +14,17 @@ class GameBoard
         puts "\n #{@positions[0] } | #{@positions[1]} | #{@positions[2]} \n---|---|---\n #{@positions[3]} | #{@positions[4]} | #{@positions[5]} \n---|---|---\n #{@positions[6]} | #{@positions[7]} | #{@positions[8]}"
     end
 
-    def winner? player_positions
-        solutions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-        solutions.any? do |sub_array|
-            sub_array.all? do |sub_array_item|
-                player_positions.include? sub_array_item
+    def winner_or_tie? player_positions=nil , toggle
+        case toggle
+        when 'winner?'
+            solutions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+            solutions.any? do |sub_array|
+                sub_array.all? do |sub_array_item|
+                    player_positions.include? sub_array_item
+                end
             end
+        when 'tie?'
+            positions.empty? 
         end
     end
 
